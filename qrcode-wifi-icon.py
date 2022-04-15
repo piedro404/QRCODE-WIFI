22fedb69c4934563e47427650685e18f0d21effb
Pedro Henrique Martins Borges <88720549+PiedroCraft@users.noreply.github.com>
2022-04-15 17:31:03 -0300

    Create qrcode-wifi-icon.py

---
+++ qrcode-wifi-icon.py
@@ -0,0 +1,21 @@
+import qrcode
+from getpass import getpass
+import os
+from qrcode.image.styledpil import StyledPilImage
+
+try:
+    type = "WPA"
+    ssid = input("SSID(Nome da Rede): ")
+    password = getpass("Password(Senha): ")
+    hidden = False
+    qr = qrcode.QRCode(version=3, error_correction=qrcode.constants.ERROR_CORRECT_H)
+
+    if not os.path.exists("QRCODE"):
+        os.makedirs("QRCODE")
+    qr.add_data(f"WIFI:T:{type};S:{ssid};P:{password};H:{hidden};;")
+    img = qr.make_image(image_factory=StyledPilImage, embeded_image_path="imagem/signal.png")
+    img.save(f"QRCODE/{ssid.upper()}.png")
+    print("QRCODE Salvo com Sucesso")
+
+except:
+    print("\nOcorreu algum erro!")
